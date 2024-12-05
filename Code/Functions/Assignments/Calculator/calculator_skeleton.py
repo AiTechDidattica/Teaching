e38c709e1fd285b82a4fac597de8110b5eefa969
AitechDidattica <didattica@aitechstuff.com>
2024-12-05 10:03:17 +0100

    Update calculator_skeleton.py

--- Code/Functions/Assignments/Calculator/calculator_skeleton.py
+++ Code/Functions/Assignments/Calculator/calculator_skeleton.py
@@ -1,16 +1,23 @@
 def somma(a, b):
-
+   pass
+   
 def differenza(a, b):
+   pass
    
 def moltiplicazione(a, b):
+   pass
   
 def divisione(a, b):
+   pass
    
 def check_operazione():
+   pass
     
 def check_input(prompt):
+   pass
     
 def main():
+   pass
     
 if __name__ == '__main__':
     main()
